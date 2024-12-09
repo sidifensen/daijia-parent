acfd075f4eaabd0dede000764339c6b99e2d4c46
lxt <1848221808@qq.com>
2024-12-09 13:29:19 +0800

    Changes

--- model/src/main/java/com/atguigu/daijia/model/enums/OrderStatus.java
+++ model/src/main/java/com/atguigu/daijia/model/enums/OrderStatus.java
@@ -5,16 +5,15 @@ import lombok.Getter;
 
 @Getter
 public enum OrderStatus {
-    // 这个订单状态是order_status_log表的的专属状态,不是order_info的status
     WAITING_ACCEPT(1, "等待接单"),
     ACCEPTED(2, "已接单"),
     DRIVER_ARRIVED(3, "司机已到达"),
     UPDATE_CART_INFO(4, "更新代驾车辆信息"),
     START_SERVICE(5, "开始服务"),
-    END_SERVICE(6, "结束服务,未付款"),//未付款
+    END_SERVICE(6, "结束服务"),
     UNPAID(7, "待付款"),
     PAID(8, "已付款"),
-    FINISH(9, "完成"),
+    FINISH(9, "完成"),//没有接口调用
     CANCEL_ORDER(-1, "未接单取消订单"),
     NULL_ORDER(-100, "不存在"),
     ;
